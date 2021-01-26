9c2c6a5ea2228d2342f39f76a82133b6d04d5153
prgrmmrthz <izayoixd@gmail.com>
2021-01-26 21:14:26 +0800

    created product migration

---
+++ database/migrations/2021_01_26_125343_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 100);
+            $table->float('regular_price', 8,2)->nullable();
+            $table->boolean('is_for_sale');
+            $table->integer('available_stock')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
